d452ea93e5ce4382cbc3605803ab0e97d784dc40
CJBiohacker <cj.moyses@gmail.com>
2021-06-08 03:04:05 -0300

    '1º Codigo Java Script'

--- JavaScript/1o-codigo-javascript.js
+++ JavaScript/1o-codigo-javascript.js
@@ -1,1 +1,1 @@
-console.log("PRIMEIRO CÓDIGO EM JAVASCRIPT CARAIOOOO !!!"); // O Objeto 'console' fornece acesso ao console de debug do navegador. E o Método 'log' imprime uma mensagem no console.
+console.log("PRIMEIRO CÓDIGO EM JAVASCRIPT CARAIOOOOO !!!"); // O Objeto 'console' fornece acesso ao console de debug do navegador. E o Método 'log' imprime uma mensagem no console.
