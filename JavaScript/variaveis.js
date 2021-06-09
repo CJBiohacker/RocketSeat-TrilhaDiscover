9e51e62896560417ad90d41556deca6e81b660d9
CJBiohacker <cj.moyses@gmail.com>
2021-06-09 00:24:34 -0300

    'Declaraçao de Variaveis e Tipagem'

--- JavaScript/variaveis.js
+++ JavaScript/variaveis.js
@@ -1,24 +1,26 @@
 /* Var, Let e Const */
-/*  Var - O valor declarado à variável é global, ou seja, aplica-se ao código inteiro. A última modificação/declaração será contada como a atual.
-    Let - O valor declarado à variável é local, ou seja, aplica-se somente no bloco em que faz parte.
+/*  Var - O valor declarado à variável é global, ou seja, aplica-se ao código inteiro. A última atribuição/modificação será contada como a atual.
+    Let - O valor declarado à variável é local, ou seja, aplica-se somente no bloco em que faz parte. Caso
+for declarada de forma global, qualquer tentativa de atribuição/modificação acarretará em erro na hora de execução do código.
     Const - O valor declarao à uma constante é global e imutável. Qualquer tentativa de modificà-la
-acarretará em erro na hora de execução do código. */
+acarretará em erro na hora de execução do código.
+*/
 
-var nome = "Carlos"
-console.log(nome);  {
-    nome = "Carlos Junior"
+var nome = "Carlos";
+    console.log(nome);  {
+    nome = "Carlos Junior";
 }
-console.log(nome);
+    console.log(nome);
 
-let idade = "31"
-console.log(idade); {
-    idade = "32"
+let idade = "31";
+    console.log(idade); {
+    idade = "32";
     console.log(idade);
 }
-console.log(idade);
+    console.log(idade);
 
-const altura = 1.68
-console.log(altura);
+const altura = 1.68;
+    console.log(altura);
 // altura = 1.70
 // console.log(altura);
 
@@ -32,20 +34,24 @@ Conforme se atribui o valor, naturalmente o tipo é identificado. (característi
 
 // var cor:String = "Vermelho"; // Ocorre um erro de Sintaxe ao definir a variável como String
 
-var cor = "Vermelho"
-console.log("Valor:" + cor + "  Tipo:" + typeof cor);
+var cor = "Vermelho";
+    console.log("Valor:" + cor + "  Tipo:" + typeof cor);
+    cor = "Verde";
+    console.log("Valor:" + cor + "  Tipo:" + typeof cor);
+    cor = true;
+    console.log("Valor:" + cor + "  Tipo:" + typeof cor);
+    cor = 10.25;    
+    console.log("Valor:" + cor + "  Tipo:" + typeof cor);
+    cor = 1.8*10**308;
+    console.log("Valor:" + cor + "  Tipo:" + typeof cor);
 
-var cor = "Verde"
-console.log("Valor:" + cor + "  Tipo:" + typeof cor);
+let olhocor = "Azul";
+    console.log("Valor:" + olhocor + "  Tipo:" + typeof olhocor);
+    olhocor = "Castanho-escuro";
+    console.log("Valor:" + olhocor + "  Tipo:" + typeof olhocor);
 
-var cor = true
-console.log("Valor:" + cor + "  Tipo:" + typeof cor);
+const cabelocor = "Castanho-escuro";
+console.log("Valor:" + cabelocor + "  Tipo:" + typeof cabelocor);
 
-var cor = 10.25
-console.log("Valor:" + cor + "  Tipo:" + typeof cor);
-
-var cor = 1.8*10**308
-console.log("Valor:" + cor + "  Tipo:" + typeof cor);
-
-
-
+/* Scope e Block Statement */
+/* Scope - */
