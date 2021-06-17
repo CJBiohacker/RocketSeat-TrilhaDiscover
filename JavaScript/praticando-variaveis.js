45606ae75bd987b6a2c7253c27a6f26326ad0d77
CJBiohacker <cj.moyses@gmail.com>
2021-06-17 10:46:54 -0300

    'Praticando com Variaveis'

---
+++ JavaScript/praticando-variaveis.js
@@ -0,0 +1,20 @@
+//  Declaração de variável
+var nome;
+
+// Atribuição de valor à variável
+nome = "CJ";
+
+// Imprime o tipo de dado atribuído
+console.log(typeof(nome));
+
+// Agrupamento de variáveis do mesmo tipo de declaração
+let idade, isHuman;
+
+// Atribuição de valor às variáveis (Number Integer e Boolean)
+idade = 32;
+isHuman = true;
+
+// Concatenação entre mensagem e variável utilizando ',' e '+'. Permite a impressão de várias variáveis.
+console.log("Nome: " , nome, ", Idade: " , idade, ", Humano? " , isHuman);
+console.log(`Tipo da variável 'isHuman': `, typeof(isHuman),
+`Tipo da variável 'nome': `, typeof(name));
