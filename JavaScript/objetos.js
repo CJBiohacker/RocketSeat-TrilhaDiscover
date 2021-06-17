fda8a55eb09cfb34ab68336a42e8ec2ea426d4e5
CJBiohacker <cj.moyses@gmail.com>
2021-06-17 13:56:28 -0300

    'Criaçao de Objetos'

--- JavaScript/objetos.js
+++ JavaScript/objetos.js
@@ -6,4 +6,17 @@ const mutante = {
     bruxo: true
 }
 
+// Imprimindo o objeto
 console.log(mutante);
+
+// Selecionando atributos do objeto para impressão
+console.log(`Qual a idade de ${mutante.nome}? ${mutante.idade}`);
+
+// Aplicando uma Estrutura Condicional (If.. else..) no atributo boolean do objeto
+if (mutante.bruxo = true) {
+    console.log(`Qual o nome do mutante? ${mutante.nome}. Ele é um bruxo ? Correto`);
+} else {
+    console.log(`Qual o nome do mutante? ${mutante.nome}. Ele é um bruxo ? Falso`);
+};
+
+
