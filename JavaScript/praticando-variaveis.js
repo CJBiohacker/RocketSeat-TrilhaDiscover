32cc3bcce4c500adec2176ad6c1b747b625e36b7
CJBiohacker <cj.moyses@gmail.com>
2021-06-17 11:20:50 -0300

    'Praticando com Variaveis'

--- JavaScript/praticando-variaveis.js
+++ JavaScript/praticando-variaveis.js
@@ -14,7 +14,15 @@ let idade, isHuman;
 idade = 32;
 isHuman = true;
 
-// Concatenação entre mensagem e variável utilizando ',' e '+'. Permite a impressão de várias variáveis.
-console.log("Nome: " , nome, ", Idade: " , idade, ", Humano? " , isHuman);
-console.log(`Tipo da variável 'isHuman': `, typeof(isHuman),
-`Tipo da variável 'nome': `, typeof(name));
+// Concatenação entre mensagem e variável utilizando '+' , ',' e '${}'. Permite a impressão de várias variáveis em apenas uma linha do 'console.log'.
+// Utilizando o '+', é necessário adicionar um espaço antes de fechar uma string para melhor organização do texto.
+console.log("Nome: " + nome, "| Idade: " + idade, "| Humano? " + isHuman); 
+
+// Utilizando a ',' ocorre um espaçamento natural entre as strings e variáveis impressas.
+console.log(`Tipo da variável 'isHuman':`,typeof(isHuman),
+`| Tipo da variável 'nome':`, typeof(name));
+
+// Utilizando o '${}' é a melhor forma, pois permite a organização do texto e das variáveis dentro do 'console.log' sem preocupação com espaçamento baseado em sintaxe.
+console.log(`Nome: ${nome} | Idade: ${idade} |`);
+
+
