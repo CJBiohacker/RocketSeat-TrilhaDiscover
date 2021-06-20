954e25508f9355ae75e56f356680a523ec36df86
CJBiohacker <cj.moyses@gmail.com>
2021-06-20 18:15:44 -0300

    'Criaçao e Manipulaçao de Vetor'

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -17,7 +17,7 @@ const bestiary = [
 console.log(`Número de Criaturas do Bestiário: ${bestiary.length}`) // Retorna o total de elementos do vetor.
 console.log(`Criatura 0 = ${bestiary[0]}`); // Retorna o elemento especificado pelo índice do vetor(0-Max).
 
-// No JavaScript é possível adicionar Objetos como elementos dentro de um Vetor (Array), inclusive junto de elementos simples (o que não é recomendado por razão de Boas Práticas). Para isso é só aplicar a sintaxe padrão de criação de objetos dentro do Vetor.
+// No JavaScript é possível adicionar 'Objetos' como elementos dentro de um Vetor (Array), inclusive junto de elementos simples (o que não é recomendado por razão de Boas Práticas). Para isso é só aplicar a sintaxe padrão de criação de objetos dentro do Vetor.
 
 const journal = [
     {   nome:"Ken", idade:56, nasc:"EUA"    },
