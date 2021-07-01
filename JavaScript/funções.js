a6e188a914e6d0d2b40586398a1973cec72824d1
CJBiohacker <cj.moyses@gmail.com>
2021-06-30 22:20:46 -0300

    'Funçoes JavaScript'

---
+++ JavaScript/funções.js
@@ -0,0 +1,17 @@
+// Criação de funções em JavaScript
+/* Através do comando function chamamos a estrutura que constrói funções.
+Temos de declarar os dados que permitem ela a ser executada. Chamado também de
+Function Statement. */
+
+function criadorFrases() {
+  console.log("Aula sobre funções")
+  console.log("JavaScript permite criação e execução de Funções")
+  console.log("É necessário declarar os valores e parâmetros para execução.")
+}
+
+// Após preparada a função, podemos "chamar/executar/rodar" ela no programa.
+
+criadorFrases()
+
+
+
